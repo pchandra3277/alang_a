28a291ff0b92eab24161e5ef0374c3f2b6a52957
prakashchandra007 <pchandra3277@outlook.com>
2017-12-08 09:12:48 +0530

    Snippet for Lib.. Not for Use in the Project

---
+++ Alang_s_APP/src/com/alang/bisiness/lib/Snippet.java
@@ -0,0 +1,33 @@
+package com.alang.bisiness.lib;
+
+public class Snippet {
+	E:\Java Jar Files\spring-framework-4.0.4.RELEASE-dist\spring-framework-4.0.4.RELEASE\libs\spring-aop-4.0.4.RELEASE.jar
+	E:\Java Jar Files\spring-framework-4.0.4.RELEASE-dist\spring-framework-4.0.4.RELEASE\libs\spring-aspects-4.0.4.RELEASE.jar
+	E:\Java Jar Files\spring-framework-4.0.4.RELEASE-dist\spring-framework-4.0.4.RELEASE\libs\spring-beans-4.0.4.RELEASE.jar
+	E:\Java Jar Files\spring-framework-4.0.4.RELEASE-dist\spring-framework-4.0.4.RELEASE\libs\spring-context-4.0.4.RELEASE.jar
+	E:\Java Jar Files\spring-framework-4.0.4.RELEASE-dist\spring-framework-4.0.4.RELEASE\libs\spring-core-4.0.4.RELEASE.jar
+	E:\Java Jar Files\spring-framework-4.0.4.RELEASE-dist\spring-framework-4.0.4.RELEASE\libs\spring-expression-4.0.4.RELEASE.jar
+	E:\Java Jar Files\spring-framework-4.0.4.RELEASE-dist\spring-framework-4.0.4.RELEASE\libs\spring-framework-bom-4.0.4.RELEASE.jar
+	E:\Java Jar Files\spring-framework-4.0.4.RELEASE-dist\spring-framework-4.0.4.RELEASE\libs\spring-jdbc-4.0.4.RELEASE.jar
+	E:\Java Jar Files\spring-framework-4.0.4.RELEASE-dist\spring-framework-4.0.4.RELEASE\libs\spring-jms-4.0.4.RELEASE.jar
+	E:\Java Jar Files\spring-framework-4.0.4.RELEASE-dist\spring-framework-4.0.4.RELEASE\libs\spring-messaging-4.0.4.RELEASE.jar
+	E:\Java Jar Files\spring-framework-4.0.4.RELEASE-dist\spring-framework-4.0.4.RELEASE\libs\spring-orm-4.0.4.RELEASE.jar
+	E:\Java Jar Files\spring-framework-4.0.4.RELEASE-dist\spring-framework-4.0.4.RELEASE\libs\spring-oxm-4.0.4.RELEASE.jar
+	E:\Java Jar Files\spring-framework-4.0.4.RELEASE-dist\spring-framework-4.0.4.RELEASE\libs\spring-tx-4.0.4.RELEASE.jar
+	E:\Java Jar Files\spring-framework-4.0.4.RELEASE-dist\spring-framework-4.0.4.RELEASE\libs\spring-web-4.0.4.RELEASE.jar
+	E:\Java Jar Files\spring-framework-4.0.4.RELEASE-dist\spring-framework-4.0.4.RELEASE\libs\spring-webmvc-4.0.4.RELEASE.jar
+	E:\Java Jar Files\hibernate-release-4.3.6.Final\lib\envers\hibernate-envers-4.3.6.Final.jar
+	E:\Java Jar Files\hibernate-release-4.3.6.Final\lib\jpa\hibernate-entitymanager-4.3.6.Final.jar
+	E:\Java Jar Files\hibernate-release-4.3.6.Final\lib\required\antlr-2.7.7.jar
+	E:\Java Jar Files\hibernate-release-4.3.6.Final\lib\required\dom4j-1.6.1.jar
+	E:\Java Jar Files\hibernate-release-4.3.6.Final\lib\required\hibernate-commons-annotations-4.0.5.Final.jar
+	E:\Java Jar Files\hibernate-release-4.3.6.Final\lib\required\hibernate-core-4.3.6.Final.jar
+	E:\Java Jar Files\hibernate-release-4.3.6.Final\lib\required\hibernate-jpa-2.1-api-1.0.0.Final.jar
+	E:\Java Jar Files\hibernate-release-4.3.6.Final\lib\required\jandex-1.1.0.Final.jar
+	E:\Java Jar Files\hibernate-release-4.3.6.Final\lib\required\javassist-3.18.1-GA.jar
+	E:\Java Jar Files\hibernate-release-4.3.6.Final\lib\optional\ehcache\ehcache-core-2.4.3.jar
+	E:\Java Jar Files\hibernate-release-4.3.6.Final\lib\optional\ehcache\hibernate-ehcache-4.3.6.Final.jar
+	E:\Java Jar Files\hibernate-release-4.3.6.Final\lib\optional\ehcache\slf4j-api-1.6.1.jar
+	E:\Java Jar Files\hibernate-release-4.3.6.Final\lib\jpa-metamodel-generator\hibernate-jpamodelgen-4.3.6.Final.jar
+}
+
